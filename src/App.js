4f152137c192ad40ad49e8a23d30b8697bc5fae9
Gillian <gillian@yeomans.org.uk>
2020-01-16 13:11:07 +0000

    rename components with lower case letters, add basic info to player

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 import LoginContainer from "./components/LoginContainer";
 import GameContainer from "./components/GameContainer";
 
 function App() {
   const [user, setUser] = React.useState(null);
+  console.log(user);
 
-  return user ? (
-    <GameContainer user={user} />
-  ) : (
-    <LoginContainer user={user} setUser={setUser} />
+  return (
+    <div>
+      <Header />
+      {user ? (
+        <GameContainer user={user} />
+      ) : (
+        <LoginContainer user={user} setUser={setUser} />
+      )}
+    </div>
   );
 }
 
--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -2,8 +2,14 @@ import React from "react";
 import Player from "./Player";
 import TextOverlay from "./TextOverlay";
 
+//user is an object which contains the github login and the avatar url
 const GameContainer = ({ user }) => {
-  return <p>Hi I'm the Game Container</p>;
+  return (
+    <section>
+      <Player user={user} />
+      <Player user={{ login: "DummyUser", avatar_url: "url" }} />
+    </section>
+  );
 };
 
 export default GameContainer;
