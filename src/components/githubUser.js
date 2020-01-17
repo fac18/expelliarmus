1d6d3ccab5c1311d483edd47053b95684b62fb15
Gillian <gillian@yeomans.org.uk>
2020-01-17 11:57:08 +0000

    add different backgrounds for login or game

--- src/components/githubUser.js
+++ src/components/githubUser.js
@@ -6,11 +6,11 @@ const GithubUser = ({ login, avatar_url, setUser }) => {
     <li
       onClick={event => {
         setUser({ login, avatar_url });
-        event.target.classList.add("user-selected");
+        // event.target.classList.add("user-selected");
       }}
     >
       <h3>{login}</h3>
-      <img src={avatar_url} alt="selected github user"/>
+      <img src={avatar_url} alt="selected github user" />
     </li>
   );
 };
